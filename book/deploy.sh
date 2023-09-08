192176db965859a8c7a8f6530d958529002326cd
CHEN Zhongpu <chenloveit@gmail.com>
2023-09-08 17:19:27 +0800

    auto build while deploying the books

--- book/deploy.sh
+++ book/deploy.sh
@@ -1,9 +1,10 @@
-  cd book
-  git init
-  git branch -M pages
-  touch .gitignore
-  echo ".DS_Store" >> .gitignore
-  git add .
-  git commit -m 'deploy'
-  git remote add origin git@github.com:ChenZhongPu/data-structure-swufe.git
-  git push -u -f origin pages
+mdbook build
+cd book
+git init
+git branch -M pages
+touch .gitignore
+echo ".DS_Store" >>.gitignore
+git add .
+git commit -m 'deploy'
+git remote add origin git@github.com:ChenZhongPu/data-structure-swufe.git
+git push -u -f origin pages
